5a80e2fbb3b6856c8c261bcd8e3aaf77c3f88ea7
Dries C <coenendries3@gmail.com>
2024-02-15 01:12:14 +0100

    Don't prebuild bindings

---
+++ src/NetherGames/Quiche/bindings/generate.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+use FFIMe\FFIMe;
+
+if(!file_exists(__DIR__ . "/quiche.php")){
+    $quichePath = $argv[1] ?? getenv("QUICHE_PATH");
+    if ($quichePath === false || !is_file($quichePath)) {
+        echo "Quiche path not found\n";
+        exit(1);
+    } else {
+        define("QUICHE_PATH", $quichePath);
+    }
+
+    $quiche = (new FFIMe(QUICHE_PATH, [
+        __DIR__,
+    ]))->include("bindings.h");
+
+    file_put_contents(__DIR__ . "/quiche.php", "<?php declare(strict_types=1);\n" . $quiche->compile('NetherGames\Quiche\bindings\Quiche', ($argv[2] ?? "") === "dev"));
+}
+
+require __DIR__ . "/quiche.php";
